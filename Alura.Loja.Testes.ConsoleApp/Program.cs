e8dcfbbf6fff6c669395f480eea36b23ffed59f4
yanomarcos <yanomarcos@gmail.com>
2019-04-21 00:33:28 -0300

    corrigindo o modelo para recuperar objetos relacionados

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -15,6 +15,35 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
+            using (var contexto = new LojaContext())
+            {
+                var serviceProvider = contexto.GetInfrastructure<IServiceProvider>();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                var cliente = contexto
+                    .Clientes
+                    .Include(c => c.EnderecoDeEntrega)
+                    .FirstOrDefault();
+
+                Console.WriteLine($"Endereço da entrega: { cliente.EnderecoDeEntrega.Logradouro }");
+
+                var produto = contexto
+                    .Produtos
+                    .Include(p => p.Compras)
+                    .Where(p => p.Id == 3002)
+                    .FirstOrDefault();
+
+                Console.WriteLine($"Mostrando as compras do produto {produto.Nome}");
+                foreach (var item in produto.Compras)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        private static void ExibeProdutosDaPromocao()
+        {
             using (var contexto2 = new LojaContext())
             {
                 var serviceProvider = contexto2.GetInfrastructure<IServiceProvider>();
